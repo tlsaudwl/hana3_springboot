4f2438eb6b9fae319b19d7b82b69db3165bd4f74
tlsaudwl <tlsaudwl1821@naver.com>
2024-03-25 10:12:19 +0900

    springboot 설정

---
+++ Ex01FirstApp/src/main/java/com/study/Ex01FirstApp/Ex01FirstAppApplication.java
@@ -0,0 +1,14 @@
+package com.study.Ex01FirstApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ex01FirstAppApplication {
+
+	public static void main(String[] args) {
+		System.out.println("Hello Springboot!!");
+		SpringApplication.run(Ex01FirstAppApplication.class, args);
+	}
+
+}
