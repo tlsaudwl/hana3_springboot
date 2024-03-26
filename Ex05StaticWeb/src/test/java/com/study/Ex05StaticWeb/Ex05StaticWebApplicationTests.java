98f28adb6d4ff955c8028115a709dc7f59d3e505
tlsaudwl <tlsaudwl1821@naver.com>
2024-03-26 09:30:38 +0900

    ex05

---
+++ Ex05StaticWeb/src/test/java/com/study/Ex05StaticWeb/Ex05StaticWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex05StaticWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex05StaticWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
