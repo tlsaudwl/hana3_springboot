56425b04d92eda07c6303709b85278f04e3135b1
tlsaudwl <tlsaudwl1821@naver.com>
2024-03-26 16:12:51 +0900

    ex08

---
+++ Ex08Param/src/test/java/com/study/Ex08Param/Ex08ParamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex08Param;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex08ParamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
