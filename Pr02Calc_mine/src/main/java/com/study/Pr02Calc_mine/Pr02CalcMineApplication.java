d1e840a516faf7431f1b302efb7bb20d957297ae
tlsaudwl <tlsaudwl1821@naver.com>
2024-03-27 15:20:39 +0900

    pr02 내꺼

---
+++ Pr02Calc_mine/src/main/java/com/study/Pr02Calc_mine/Pr02CalcMineApplication.java
@@ -0,0 +1,13 @@
+package com.study.Pr02Calc_mine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pr02CalcMineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pr02CalcMineApplication.class, args);
+	}
+
+}
---
+++ Pr02Calc_mine/src/test/java/com/study/Pr02Calc_mine/Pr02CalcMineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Pr02Calc_mine;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pr02CalcMineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
