dd34a91b9bb47ffabf9e3c52c50fda5671ff03e4
tlsaudwl <tlsaudwl1821@naver.com>
2024-04-08 11:07:47 +0900

    ex16

---
+++ Ex16ThymeleafLayout/src/main/java/com/study/Ex16ThymeleafLayout/MainController.java
@@ -0,0 +1,16 @@
+package com.study.Ex16ThymeleafLayout;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main(){
+        return "first";  // first.html로 응답
+    }
+    @GetMapping("/second")
+    public String second(){
+        return "second";  // second.html로 응답
+    }
+}
