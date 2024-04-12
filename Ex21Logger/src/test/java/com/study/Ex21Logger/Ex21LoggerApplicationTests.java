bd5954a57b32ce8f1aebed1b3ba7016736eecced
tlsaudwl <tlsaudwl1821@naver.com>
2024-04-12 15:58:42 +0900

    ex21

---
+++ Ex21Logger/src/test/java/com/study/Ex21Logger/Ex21LoggerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.study.Ex21Logger;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ex21LoggerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
