0d961bd147f4b4f67bd177ead79a93585b408559
tlsaudwl <tlsaudwl1821@naver.com>
2024-04-26 13:42:31 +0900

    ex27-4

---
+++ Ex27Security04/src/main/java/com/study/springboot/controller/MainController.java
@@ -0,0 +1,15 @@
+package com.study.springboot.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MainController {
+    @GetMapping("/")
+    public String main() {
+        return "index"; //index.html로 응답
+    }
+
+
+}
